8ed776d81ded7615f8086a92d0677bdd0355ca7e
taahahussainkhan <taaha128@gmail.com>
2024-01-11 00:20:58 +0500

    ExpenseDate.sccs card.jsx

--- src/components/ExpenseDate.jsx
+++ src/components/ExpenseDate.jsx
@@ -1,11 +1,13 @@
 import React from "react";
+import "./ExpenseDate.scss";
+
 
 export default function ExpenseDate(props) {
   const month = props.date.toLocaleString("en-US", { month: "long" });
   const year = props.date.getFullYear();
   const day = props.date.toLocaleString("en-US", { day: "2-digit" });
   return (
-    <div>
+    <div className="expense-date">
       <div className="month">{month}</div>
       <div className="year">{year}</div>
       <div className="day">{day}</div>
